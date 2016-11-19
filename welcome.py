9a7e87aaf1da9b7c41e1256aa246926ad107e992
Victor Goh <jgoh@wesleyan.edu>
2016-11-19 13:49:26 -0600

    fixed twilio, added bot message

--- welcome.py
+++ welcome.py
@@ -22,7 +22,7 @@ from twilio import twiml
 app = Flask(__name__)
 
 # Update with your own phone number in E.164 format
-MODERATOR = '+18609692438'
+#MODERATOR = '+18609692438'
 #End Twilio params
 
 app = Flask(__name__)
@@ -36,24 +36,20 @@ def StartCall():
     return 'About to call Twilio!'
 
 #Begin Twilio
-@app.route("/voice", methods=['GET', 'POST'])
+@app.route('/voice', methods=['GET', 'POST'])
 def call():
     """Returns TwiML for a moderated conference call"""
     # Start our TwiML response
     response = twiml.Response()
+    response.say("Welcome to the conference")
 
     # Start with a <Dial> verb
     with response.dial() as dial:
         # If the caller is our MODERATOR, then start the conference when they
         # join and end the conference when they leave
-        if request.values.get('From') == MODERATOR:
-            dial.conference(
-                'My conference',
-                startConferenceOnEnter=True,
-                endConferenceOnExit=True)
-        else:
-            # Otherwise have the caller join as a regular participant
-            dial.conference('My conference', startConferenceOnEnter=False)
+
+        # Otherwise have the caller join as a regular participant
+        dial.conference('My conference')
 
     return str(response)
 
